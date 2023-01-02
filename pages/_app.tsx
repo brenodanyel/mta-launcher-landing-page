db3fc7fe835388e123a69063660f6451fa84b696
iDz <bdls1704@hotmail.com>
2023-01-02 19:28:00 -0300

    add analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,13 @@
 import type { AppProps } from 'next/app';
+import { Analytics } from '@vercel/analytics/react';
+
 import MUIWrapper from './wrappers/mui.wrapper';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <MUIWrapper>
       <Component {...pageProps} />
+      <Analytics />
     </MUIWrapper>
   );
 }
